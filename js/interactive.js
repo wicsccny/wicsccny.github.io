f026c6d365dcaec1e116fe471e1703fa70e00597
chrystalmingo <47184930+chrystalmingo@users.noreply.github.com>
2020-09-17 00:28:46 -0400

    Created JS folder

---
+++ js/interactive.js
@@ -0,0 +1,25 @@
+
+//Scribe Button JS
+
+var $button = document.querySelector('.button');
+$button.addEventListener('click', function() {
+  var duration = 0.3,
+      delay = 0.08;
+  TweenMax.to($button, duration, {scaleY: 1.6, ease: Expo.easeOut});
+  TweenMax.to($button, duration, {scaleX: 1.2, scaleY: 1, ease: Back.easeOut, easeParams: [3], delay: delay});
+  TweenMax.to($button, duration * 1.25, {scaleX: 1, scaleY: 1, ease: Back.easeOut, easeParams: [6], delay: delay * 3 });
+});
+
+
+
+//scrolling JS
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
+
+
+
+
